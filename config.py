8a2b2d19270c881cc1ea9e3315065bb31994320a
Sujal080 <chavdasujal08@gmail.com>
2025-06-05 22:18:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ API_ID = int(os.environ.get("API_ID", "26330942"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH", "5de9fd033aa828dfd3bf0c28adeee660")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7460074981:AAH3xfM8LwbSaxqh9HMW_1QKfYXSHKZBRU0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7363509425:AAF92zS7bv50rpuH6NtlaCN-BOf1GmTaMAM")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("@Sujalnnbot")
+BOT_USERNAME = os.environ.get("@Sujalbbbot")
 # ------------------X------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", "6883471516"))
 # ------------------X------------------------------
@@ -20,5 +20,5 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002618428217"))
 # ------------------------------------------------
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://biklriplit:efaXfv2Ps9MRfner@cluster0.4hfu8zj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002630316307"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", ""))
 
